8e6cec9a9bcaf7a662dfe31caea7c3bc02504205
Shazia Zaman <shazia.a.zaman@gmail.com>
2018-02-18 08:19:51 -0600

    fix spelling

--- Python/source/domains.py
+++ Python/source/domains.py
@@ -4,31 +4,31 @@
 
 from colour import Color
 
-card_suites = ['Spade','Diamond','Heart','Club']
+card_suits = ['Spade','Diamond','Heart','Club']
 card_values = [2,3,4,5,6,7,8,9,10,'Joker','Queen','King','Ace']
 card_colors = [Color('red'),Color('black')]
 card_priority = {'Ace':1,'King':2,'Queen':3,'Joker':4}
 
 class Card:
     def __init__(self, suite, color, value, priority):
-        self.suite = suite
+        self.suit = suite
         self.color = color
         self.value = value
         self.priority = priority
     
     def __str__(self):
-        return 'Suite: ' + str(self.suite) + ' | color: ' + str(self.color) + ' | value: ' + str(self.value) + ' | priority: ' + str(self.priority)
+        return 'Suite: ' + str(self.suit) + ' | color: ' + str(self.color) + ' | value: ' + str(self.value) + ' | priority: ' + str(self.priority)
 
 class Deck:
     def __init__(self):
         self.deck = []
         for color in card_colors:
-            for suite in card_suites:
+            for suit in card_suits:
                 for value in card_values:
                     priority = card_priority.get(value)
                     if priority is None:
                         priority = 999
-                    card = Card(suite, color, value, priority)
+                    card = Card(suit, color, value, priority)
                     self.deck.append(card)
     
     def show(self):
