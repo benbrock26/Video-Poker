1ceefa09329e44fa241c61113272c7642144a9fe
benbrock26 <benbroc@gmail.com>
2018-02-19 16:42:11 -0500

    Add Player.py Boiler plate class implementation

---
+++ Python/source/ben/Player.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 19 16:20:36 2018
+
+@author: Ben Brock and Shazia Zaman
+"""
+
+from Card import Card
+from Hand import Hand
+
+class Player(object):
+    
+     def __init__(self):
+        pass
+    
+    def __del__(self):
+        pass
+    
+    def add_card(self, new_card):
+        pass
+    
+    def add_funds(self, amount_of_funds):
+        pass
+    
+    def remove_funds(self, amount_of_funds):
+        pass
+    
+    def get_funds(self):
+        pass
+    
+    def get_bet_amount(self):
+        pass
+    
+    def set_bet_amount(self, bet_amount):
+        pass
+    
+    def get_name(self):
+        pass
+    
+    '''
+    five_card_stud_hand
+    '''
+    def get_hand(self):
+        pass
+    
+    def reset(self):
+        pass
+    
+    
+## Unit Test of the Player Class ####
+def main():
+    
+    
+            
+if __name__ == '__main__':
+    main()
+    
+        
+    
+    
