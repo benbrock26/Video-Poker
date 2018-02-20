a40bed4d80a29439e0ef371579e2b539a8cf2f26
benbrock26 <benbroc@gmail.com>
2018-02-19 22:55:59 -0500

    Add Python Poker.py Boilerplate.
    Add Python Poker.py Boilerplate.  Will finish up the code later.  Good starting point for now.

---
+++ Python/source/ben/Poker.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 19 21:58:28 2018
+
+@author: Ben Brock and Shazia Zaman
+"""
+
+from Deck import Deck
+from Player import Player
+
+
+'''
+Starting Boilerplate for the Poker class.
+
+This is where the most complex code will reside for the Poker game.
+
+'''
+class Poker(object):
+    
+    def __init__(self):
+        pass
+    
+    def __del__(self):
+        pass
+    
+    def add_view(self, game_view):
+        pass
+    
+    def add_player(self, player):
+        pass
+    
+    def play(self):
+        pass
+    
+    def get_player_funds(self):
+        pass
+    
+    def get_player_hand(self):
+        pass
+    
+    def is_bet_valid(self, amount):
+        pass
+    
+    def get_straight_type(self, values):
+        pass
+    
+    def get_kind(self, values):
+        pass
+    
+    def reset_table(self):
+        pass
+    
+## Unit Test of the Poker Class ####
+def main():
+    
+    print "\nUnit Testing of the Poker Class.....\n"
+    
+    print "\n.... This is where most of the complex logic will be located for the Poker Game...\n"
+    
+    
+    
+            
+if __name__ == '__main__':
+    main()
